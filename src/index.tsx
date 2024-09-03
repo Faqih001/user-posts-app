1b73e82a78bc22a1f5d082d1a5a74f07d9715261
Amirul Faqih <fakiiahmad001@gmail.com>
2024-09-03 18:48:46 +0300

    Refactor UserList component to render user list only if data is available

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
 
+// Root component renders the App component inside the root element
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
